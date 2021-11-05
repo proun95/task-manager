8f556317e9761ff1cba539793e7aafc87c62b4eb
Aleksandr Sirotin <proun95@gmail.com>
2021-11-05 21:30:35 +0400

    add tests for task

--- test/factories/sequence.rb
+++ test/factories/sequence.rb
@@ -1,9 +1,13 @@
 FactoryBot.define do
-  sequence :string, aliases: [:first_name, :last_name, :password, :avatar] do |n|
+  sequence :string, aliases: [:name, :first_name, :last_name, :description, :password, :avatar] do |n|
     "string#{n}"
   end
 
   sequence :email do |n|
     "string#{n}@example.com"
+  end
+
+  sequence :expired_at do
+    10.days.after
   end
 end
