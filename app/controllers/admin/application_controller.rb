131e8469cf667ce72b261fa005b08e9f491d8528
Aleksandr Sirotin <proun95@gmail.com>
2021-11-12 21:10:12 +0400

    add seeds, execute rubocop autocorrect

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,11 +1,11 @@
 class Admin::ApplicationController < ApplicationController
   include AuthHelper
   helper_method :current_user
-  
+
   before_action :authenticate_user!, :authorize
 
   def authorize
-    render(:file => File.join(Rails.root, 'public/403.html'), :status => 403, :layout => false) if (forbidden?)
+    render(file: File.join(Rails.root, 'public/403.html'), status: 403, layout: false) if forbidden?
   end
 
   def forbidden?
