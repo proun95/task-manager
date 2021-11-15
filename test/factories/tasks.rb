41b0861c7f6d326b019de7d8d0749fbfb8b8d475
Aleksandr Sirotin <proun95@gmail.com>
2021-11-16 00:51:34 +0400

    fix tasks factory

--- test/factories/tasks.rb
+++ test/factories/tasks.rb
@@ -4,7 +4,6 @@ FactoryBot.define do
     description
     author factory: :manager
     assignee factory: :developer
-    state { Task.state_machine.initial_state(0).name }
     expired_at
   end
 end
