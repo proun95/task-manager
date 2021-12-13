02ccb89350935af60fbc8b26c66f5cc9568ef947
Aleksandr Sirotin <proun95@gmail.com>
2021-12-13 10:07:27 +0400

    add MUITheme

--- app/javascript/App.js
+++ app/javascript/App.js
@@ -3,11 +3,14 @@ import React from 'react';
 import store from 'store';
 import { Provider } from 'react-redux';
 import TaskBoard from 'containers/TaskBoard';
-// import MUITheme from 'MUITheme/MUITheme';
+import MUITheme from 'MUITheme/MUITheme';
+import { ThemeProvider } from '@material-ui/styles';
 
 const App = () => (
   <Provider store={store}>
-    <TaskBoard />
+    <ThemeProvider theme={MUITheme}>
+      <TaskBoard />
+    </ThemeProvider>
   </Provider>
 );
 
---
+++ app/javascript/MUITheme/MUITheme.js
@@ -0,0 +1,14 @@
+import { createTheme } from '@material-ui/core/styles';
+
+const MUITheme = createTheme({
+  palette: {
+    primary: {
+      main: '#0caa41',
+    },
+    secondary: {
+      main: '#757575',
+    },
+  },
+});
+
+export default MUITheme;
