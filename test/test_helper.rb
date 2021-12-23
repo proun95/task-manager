79bbfa9a3ce727c549fce81afced9340f4533339
Aleksandr Sirotin <proun95@gmail.com>
2021-12-23 11:18:07 +0400

    add coveralls

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,8 +2,10 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 require 'simplecov'
+require 'coveralls'
 
 SimpleCov.start
+Coveralls.wear!
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
